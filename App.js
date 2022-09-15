ae6d13e9cbd85ef4edb98f99ca353babdc957246
manuel-hg <manuel_hg_1@outlook.com>
2022-09-14 22:11:10 -0500

    api v1

--- App.js
+++ App.js
@@ -1,12 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StatusBar, StyleSheet, Text, View, ActivityIndicator, FlatList, TouchableOpacity } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+
 
 export default function App() {
+
+  useEffect(() => {
+    getInfo();
+  },[]);
+
+  const [initialState, setInitialState] = useState({
+    texto: '',
+    data: [],
+    isLoading: true
+  });
+  const { texto, data, isLoading } = initialState;
+
+  const handleSetText = (e) => {
+    setInitialState({ ...initialState, texto: e })
+  }
+
+  const getInfo = async () => {
+    console.log("entra a fun")
+    try {
+      console.log("entra a try")
+      const request = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const response = await request.json();
+      const mapeo = response.results.map((a,b,c) => (a) && ({...a, id:b+1}));
+      setInitialState({ ...initialState, data: mapeo, isLoading: false })
+      
+    } catch (error) {
+      console.log(error)
+      setInitialState({ ...initialState, isLoading: false })
+    }
+  }
+
+const Item = ({name, id}) =>{
+  return(
+    <TouchableOpacity>
+      <View style={{ flex: 1, alignSelf: "stretch", borderBottomWidth: 1, borderColor: '#DADADA', height: 45, justifyContent: 'center', paddingLeft: 10, backgroundColor: (parseInt(id) % 2) === 0 ? '#F7F7F7' : 'white' }}>
+        <Text style={{ fontSize: 22 }}>{id} - {name}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light-content" />
+      <View style={styles.container}>
+        <View style={{ height: 'auto', alignSelf: 'stretch' }}>
+          <SearchBar
+            round
+            searchIcon={{ size: 24 }}
+            placeholder="Buscar..."
+            onChangeText={(e) => handleSetText(e)}
+            onClear={() => setInitialState({ ...initialState, texto: '' })}
+            value={texto}
+          />
+        </View>
+        <View style={{ flex: 1, alignSelf: "stretch", justifyContent: 'center', alignItems: 'center' }}>
+          {
+          isLoading
+          ?
+            <ActivityIndicator size="large" color="#D7D7D7" />
+          :
+            <FlatList
+              style={{ alignSelf: "stretch", height: 'auto' }}
+              data={data}
+              keyExtractor={item => item.name}
+              renderItem={({ item }) => <Item name={item.name} id={item.id}/>}
+            />
+          }
+        </View>
+      </View>
+    </>
   );
 }
 
